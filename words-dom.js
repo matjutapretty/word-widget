3cedac1e48289d0bdc8417976389d2d577ff830d
Pretty <mphefopretty1997@gmail.com>
2022-03-22 21:56:08 +0200

    separated words

--- words-dom.js
+++ words-dom.js
@@ -1,6 +1,3 @@
-// localStorage.setItem()
-// localStorage.getItem()
-
 const inputSentence = document.querySelector(".inputSentence")
 const analyseBtn =document.querySelector(".analyseBtn")
 const hideWords = document.getElementById("hideWords")
@@ -10,35 +7,69 @@ const sentence = document.querySelector(".list")
 const previousSentence = []
 
 function btnClicked() {
-let string = " ";
+let string = "";
 const words = inputSentence.value;
-const splitWords = words.split(" ")
+const splitWords = words.split(" ");
 
   let wordCount = words.match(/(\w+)/g).length;
   NumberOfWords.innerHTML = `Word Count : ${wordCount}`;
 
-  for(let i =0; i < splitWords.length; i++) {
+  const wordList = splitWords.map(word => {
+    return {
+      word,
+      length : word.length,
+      type : word.length > 4 ? "longer" : ""
+    }
+  });
+
+  let longestWord = {
+    length : 0
+  }
+
+  wordList.forEach((word, index) => {
+    if (word.length > longestWord.length) {
+      longestWord = {...word, index}
+    }
+  });
+
+  const longestWords = wordList.filter(word => word.length === longestWord.length);
+  wordList[longestWord.index].type = '<span>' + longestWords + '</span>'
+  
+  console.log(longestWords)
+
+  for(let i = 0; i < splitWords.length; i++) {
     const element = splitWords[i];
-    if(element.length > 4 ){
-      string += '<mark>' + element + '</mark>'
+
+    let averageLength = " ";
+
+    if(element.length >= 5 ){
+      string += '<mark> ' + element + '</mark> '
+     if (element.length > string.length) {
+       console.log(string)
+       averageLength += '<span>' + element + '</span>'
+     } 
     } else {
       string += element + " ";
-    }
+     }
   }
+
   previousSentence.push(string)
+  if (previousSentence.length <= 5) {
+    sentence.innerHTML = `Last 5 sentences : ${previousSentence},`;
+  }
   messages.innerHTML = ` ${string},`;
-  sentence.innerHTML = `Last 5 sentences : ${previousSentence}`;
 }
 
+
 hideWords.addEventListener('click', function(){
-  let mySrting = ' ';
+  let mySrting = " ";
   const longestWords = inputSentence.value.split(" ")
   if (hideWords.checked == true){
     for(let i = 0; i < longestWords.length; i++){
       const element = longestWords[i];
 
       if (element.length > 4){
-        mySrting += '<mark>' + element + '</mark>'
+        mySrting += "<mark> " + element + "</mark> "
       }
     }
   } else {
@@ -46,13 +77,13 @@ hideWords.addEventListener('click', function(){
       const element = longestWords[i];
 
       if(element.length > 4){
-        mySrting += '<mark>' + element + '</mark>'
+        mySrting += "<mark> " + element + "</mark> "
       } else {
-        mySrting += element + ' '
+        mySrting += element + " "
       }
     }
   }
-  messages.innerHTML = `${mySrting}`;
+  messages.innerHTML = `${mySrting},`;
 })
 analyseBtn.addEventListener('click', btnClicked)
 
